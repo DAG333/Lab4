32e7e0b2bb2d12471a1ac370b8839ac89896bd42
Daniel Goldberg <dag333@gmail.com>
2013-03-29 18:00:22 -0500

    Lab4
    Had to create a new repository because of detached head, could not
    commit to old

---
+++ FiveCardDraw.h
@@ -0,0 +1,18 @@
+#ifndef FiveCardDraw_h
+#define FiveCardDraw_h
+
+#include "stdafx.h"
+#include "game.h"
+using namespace std;
+
+class FiveCardDraw: public Game {
+
+protected:
+
+public:
+
+private:
+
+};
+
+#endif
